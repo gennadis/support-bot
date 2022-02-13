1f79e9b612e8500c51afd04d0635336778fe6a9f
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-13 20:09:09 +0300

    Get prepared intents from Dvmn

--- flow.py
+++ flow.py
@@ -1,5 +1,8 @@
+import json
 import os
+from pprint import pprint
 
+import requests
 from google.cloud import dialogflow
 
 
@@ -17,3 +20,16 @@ def get_flow_reply(session_id: int, user_text: str, language_code: str = "ru") -
     )
 
     return response.query_result.fulfillment_text
+
+
+def get_prepared_intents(url: str):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return json.loads(response.text)
+
+
+if __name__ == "__main__":
+    url = "https://dvmn.org/media/filer_public/a7/db/a7db66c0-1259-4dac-9726-2d1fa9c44f20/questions.json"
+    intents = get_prepared_intents(url)
+    pprint(intents)
