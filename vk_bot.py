472487623f7f92790ecae90c223a47d35e6bc76f
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-14 14:15:42 +0300

    Add Vk Errors reporting

--- vk_bot.py
+++ vk_bot.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import vk_api
@@ -7,13 +8,17 @@ from vk_api.vk_api import VkApiMethod
 from dotenv import load_dotenv
 
 from flow import get_flow_reply
+from tg_bot import TelegramLogsHandler
 
 
 def reply_with_flow_vk(event: Event, vk_api: VkApiMethod) -> None:
     user_id = event.user_id
     user_text = event.text
 
-    flow_reply = get_flow_reply(session_id=user_id, user_text=user_text)
+    try:
+        flow_reply = get_flow_reply(session_id=user_id, user_text=user_text)
+    except Exception:
+        logger.exception()
 
     if not flow_reply.intent.is_fallback:
         vk_api.messages.send(
@@ -27,6 +32,7 @@ def main(token: str):
     vk_session = vk_api.VkApi(token=token)
     vk = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
+    logger.info("📗 VK API long polling started successfully")
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
@@ -36,5 +42,14 @@ def main(token: str):
 if __name__ == "__main__":
     load_dotenv()
     vk_token = os.getenv("VK_TOKEN")
+    telegram_token = os.getenv("TELEGRAM_TOKEN")
+    telegram_admin_chat_id = os.getenv("TELEGRAM_ADMIN_CHAT_ID")
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("Logger")
+    logger = TelegramLogsHandler(
+        tg_token=telegram_token, chat_id=telegram_admin_chat_id
+    )
+    logger.info("📗 VK bot started successfully")
 
     main(token=vk_token)
