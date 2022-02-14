a37cac5b196fe254e1ec41dc62f57ab2d3edf6ba
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-14 10:54:28 +0300

    Add VK bot

---
+++ vk_bot.py
@@ -0,0 +1,25 @@
+import os
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from dotenv import load_dotenv
+
+
+def run_vk_lognpoll(token: str):
+    vk_session = vk_api.VkApi(token=token)
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print("Новое сообщение:")
+            if event.to_me:
+                print("Для меня от: ", event.user_id)
+            else:
+                print("От меня для: ", event.user_id)
+            print("Текст:", event.text)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    vk_token = os.getenv("VK_API_KEY")
+    run_vk_lognpoll(vk_token)
